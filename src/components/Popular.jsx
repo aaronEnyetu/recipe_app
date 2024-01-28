e6a1b0f67a8c0c7eb603c68fa2fbe797ad647d90
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-28 12:07:22 -0800

    displaying recipes on the page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 function Popular() {
+
+
+const [popular, setPopular] = useState([])
+
     useEffect(() => {
         getPopular()
 
@@ -10,11 +14,20 @@ function Popular() {
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
         const data = await api.json()
-        console.log(data)
+        // console.log(data)
+        setPopular(data.recipes)
 }
 
   return (
-    <div>Popular</div>
+      <div>
+          {popular.map((recipe) => {
+              return (
+                  <div key={recipe.id}>
+                      <p>{recipe.title }</p>
+                  </div>
+              )
+          })}
+    </div>
   )
 }
 
